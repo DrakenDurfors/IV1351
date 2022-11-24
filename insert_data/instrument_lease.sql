880156cf06d28ff3f67166cfeaba587634e27d64
DrakenDurfors <47025935+DrakenDurfors@users.noreply.github.com>
2022-11-24 13:58:43 +0100

    added instrument leases

--- insert_data/instrument_lease.sql
+++ insert_data/instrument_lease.sql
@@ -1,3 +1,6 @@
 INSERT INTO instrument_lease (lease_start, student_id, instruments_id)
 VALUES
-    ()
+    ('2022-11-30', 2, 6),
+    ('2022-10-05', 3, 10),
+    ('2022-09-20', 2, 1),
+    ('2022-08-17', 4, 5);
