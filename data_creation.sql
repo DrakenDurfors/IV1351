99c9ff035f892542179fa1514b74952232275c38
Vildsvinet <tove.verner.carlsson@gmail.com>
2022-11-24 11:55:00 +0100

    Create data_creation.sql

---
+++ data_creation.sql
@@ -0,0 +1,14 @@
+INSERT INTO address (postal_code, street_name, street_number)
+VALUES
+    ('11212','Grimauld Place','12'),
+    ('10010','The Burrow','1'),
+    ('14444','Privet Drive','4');
+
+INSERT INTO person (ssn,first_name,last_name,address_id,phone_no,email)
+VALUES
+    ('195911031337', 'Sirius', 'Black', 1,'0803331100', 'padfoot@gmail.com' ),
+    ('198003015555','Ron','Weasley',2,'0735554444','theweasel@hotmail.com'),
+    ('198108118888','Ginny','Weasley',2,'0737778888','badass_babe@gmail.com'),
+    ('19800731','Harry','Potter',3,'0776669999','draco_lover@yahoo.com');
+
+
