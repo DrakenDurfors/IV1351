a05700c270407e914418badbc36de7079b2b93ba
DrakenDurfors <47025935+DrakenDurfors@users.noreply.github.com>
2022-11-24 11:47:03 +0100

    Create lesson_pricing.sql

---
+++ insert_data/lesson_pricing.sql
@@ -0,0 +1,11 @@
+INSERT INTO lesson_pricing (type, level, price, discount)
+VALUES
+    ('individual','beginner',350, 10),
+    ('individual','intermediate',350, 10),
+    ('individual','advanced',450, 10),
+    ('Group','beginner',300, 5),
+    ('Group','intermediate',300, 5),
+    ('Group','advanced',400, 5),
+    ('Ensamble','beginner',250, 15),
+    ('Ensamble','intermediate',250, 15),
+    ('Ensamble','advanced',350, 15);
