20fc0d44b976f7de814d68f7a59dc09cdf1f8366
Vildsvinet <tove.verner.carlsson@gmail.com>
2022-11-24 11:58:00 +0100

    separated data generation
    one file per table

---
+++ person.sql
@@ -0,0 +1,6 @@
+INSERT INTO person (ssn,first_name,last_name,address_id,phone_no,email)
+VALUES
+    ('195911031337', 'Sirius', 'Black', 1,'0803331100', 'padfoot@gmail.com' ),
+    ('198003015555','Ron','Weasley',2,'0735554444','theweasel@hotmail.com'),
+    ('198108118888','Ginny','Weasley',2,'0737778888','badass_babe@gmail.com'),
+    ('19800731','Harry','Potter',3,'0776669999','draco_lover@yahoo.com');
