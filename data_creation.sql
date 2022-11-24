4c646119d2c5ba70bb10648e4ff2248592f3614d
Vildsvinet <tove.verner.carlsson@gmail.com>
2022-11-24 14:13:33 +0100

    Update data_creation.sql

--- data_creation.sql
+++ data_creation.sql
@@ -59,4 +59,50 @@ VALUES
     (4,1);
 
 INSERT INTO sibling_relation (student_id_1, student_id_2)
-    VALUES (2,3)
+    VALUES (2,3);
+
+INSERT INTO instructor (person_id, ensemble_teacher)
+VALUES
+    (6, 'Yes'),
+    (7, 'No');
+
+INSERT INTO instructor_proficiencies (instructor_id, instrument_id)
+VALUES
+    (6, 1),
+    (6, 3),
+    (6, 5),
+    (7, 1),
+    (7, 2),
+    (7, 4);
+
+INSERT INTO schedule_time (time, date, instructor_id)
+VALUES
+    ('10:00', '2022-11-30', 6),
+    ('13:00', '2022-11-30', 7),
+    ('15:00', '2022-11-30', 6),
+    ('09:00', '2022-12-01', 7),
+    ('10:00', '2022-12-02', 6),
+    ('10:00', '2022-12-02', 7);
+
+INSERT INTO lesson (lesson_pricing_id, place, schedule_time_id)
+VALUES
+    (1,'Ka-301', 1), --individual, beginner, instructor 6
+    (1,'Ka-301', 3), --individual, beginner, instructor 6
+    (4,'Ka-303', 2), --group, beginner, instructor 7
+    (4,'Ka-301', 4), --group, beginner, instructor 7
+    (8,'Ka-303', 6); --ensemble, advanced, instructor 7
+
+INSERT INTO ensemble_lesson (lesson_id, max_num_students, min_num_students, genre)
+VALUES
+    (5,20,2,'Heavy Metal');
+
+INSERT INTO individual_lesson (lesson_id, instrument_type_id)
+VALUES
+    (1, 1), --instructor 6, guitar
+    (2, 3); --instructor 6, piano
+
+INSERT INTO group_lesson (lesson_id, max_num_students, min_num_students, instrument_type_id)
+VALUES
+    (3,15,2,2),
+    (4,15,2,2);
+
