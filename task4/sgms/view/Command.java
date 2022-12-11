6a71a96977b25b8739c494229676e5399c35ba04
DrakenDurfors <47025935+DrakenDurfors@users.noreply.github.com>
2022-12-11 13:34:59 +0100

    Added relevant code to view
    Since he lets us use as much code as we want from his view layer I just copied over the relevant things. Still need  to create the LIST, RENT and TERMINATE commands.

--- task4/sgms/view/Command.java
+++ task4/sgms/view/Command.java
@@ -1,4 +1,31 @@
 package view;
 
-public class Command {
+/**
+ * Defines all commands that can be performed by a user of the chat application.
+ */
+public enum Command {
+    /**
+     * Lists all instruments available to rent.
+     */
+    LIST,
+    /**
+     * Rents a specified instrument to a specified student.
+     */
+    RENT,
+    /**
+     * Terminates the rental of an instrument.
+     */
+    TERMINATE,
+    /**
+     * Lists all commands.
+     */
+    HELP,
+    /**
+     * Leave the chat application.
+     */
+    QUIT,
+    /**
+     * None of the valid commands above was specified.
+     */
+    ILLEGAL_COMMAND
 }
