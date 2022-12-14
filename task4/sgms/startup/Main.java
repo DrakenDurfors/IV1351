8dfd54b7ec135fa9d2d86a5c09a6bdd4b61acfb4
DrakenDurfors <47025935+DrakenDurfors@users.noreply.github.com>
2022-12-14 20:56:13 +0100

    Fixed main exception

--- task4/sgms/startup/Main.java
+++ task4/sgms/startup/Main.java
@@ -2,15 +2,16 @@ package startup;
 
 import view.BlockInterpreter;
 import controller.Controller;
+import integration.SchoolDBException;
 
 public class Main{
 
     public static void main(String[] args){
         try {
             new BlockInterpreter(new Controller()).handleCmds();
-        } catch (Exception e) {
-            // TODO: handle exception
-
+        } catch (SchoolDBException e) {
+            System.out.println("Unable to connect to soundgood database");
+            e.printStackTrace();
         }
     }
 
